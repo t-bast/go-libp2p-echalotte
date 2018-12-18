7c37839950ad484032f5525c8c12283d2bdc37c4
t-bast <bastien@stratumn.com>
2018-12-18 17:53:15 +0100

    Find relay peers and build random circuit.

--- circuit_test.go
+++ circuit_test.go
@@ -2,20 +2,34 @@ package echalotte_test
 
 import (
 	"context"
-	"errors"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/t-bast/go-libp2p-echalotte"
 	"github.com/t-bast/go-libp2p-echalotte/mocks"
+
+	"gx/ipfs/QmY5Grm8pJdiSSVsYxx4uNRgweY72EmYwuSDbRnbFok3iY/go-libp2p-peer"
+	"gx/ipfs/QmZ9zH2FnLcxv1xyzFeUpDUeo55xEhZQHgveZijcxr7TLj/go-libp2p-peerstore"
 )
+
+// Create a test circuit builder using the given discovery mock.
+func newTestCircuitBuilder(t *testing.T, discover *mocks.MockDiscovery) *echalotte.CircuitBuilder {
+	discover.EXPECT().Advertise(gomock.Any(), echalotte.OnionRelay)
+
+	cb, err := echalotte.NewCircuitBuilder(context.Background(), discover)
+	require.NoError(t, err)
+
+	return cb
+}
 
 func TestCircuitBuilder(t *testing.T) {
 	t.Run("New()", func(t *testing.T) {
-		t.Run("forwards advertiser error", func(t *testing.T) {
+		t.Run("wraps advertiser error", func(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 
@@ -26,7 +40,7 @@ func TestCircuitBuilder(t *testing.T) {
 			)
 
 			cb, err := echalotte.NewCircuitBuilder(context.Background(), discover)
-			assert.EqualError(t, err, "fatal")
+			assert.EqualError(t, errors.Cause(err), "fatal")
 			assert.Nil(t, cb)
 		})
 
@@ -35,11 +49,117 @@ func TestCircuitBuilder(t *testing.T) {
 			defer ctrl.Finish()
 
 			discover := mocks.NewMockDiscovery(ctrl)
-			discover.EXPECT().Advertise(gomock.Any(), echalotte.OnionRelay)
-
-			cb, err := echalotte.NewCircuitBuilder(context.Background(), discover)
-			require.NoError(t, err)
+			cb := newTestCircuitBuilder(t, discover)
 			assert.NotNil(t, cb)
+		})
+	})
+
+	t.Run("Build()", func(t *testing.T) {
+		t.Run("rejects invalid circuit size", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			discover := mocks.NewMockDiscovery(ctrl)
+			cb := newTestCircuitBuilder(t, discover)
+
+			c, err := cb.Build(context.Background(), echalotte.CircuitSize(0))
+			assert.EqualError(t, err, echalotte.ErrInvalidCircuitSize)
+			assert.Nil(t, c)
+		})
+
+		t.Run("wraps find peers error", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			discover := mocks.NewMockDiscovery(ctrl)
+			cb := newTestCircuitBuilder(t, discover)
+
+			discover.EXPECT().FindPeers(gomock.Any(), echalotte.OnionRelay, gomock.Any()).Return(nil, errors.New("fatal"))
+
+			c, err := cb.Build(context.Background())
+			assert.EqualError(t, errors.Cause(err), "fatal")
+			assert.Nil(t, c)
+		})
+
+		t.Run("returns error if not enough relays discovered", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			discover := mocks.NewMockDiscovery(ctrl)
+			cb := newTestCircuitBuilder(t, discover)
+
+			// Simulate only 4 relays found whereas default circuit size is 5.
+			relaysChan := make(chan peerstore.PeerInfo)
+			go func() {
+				for i := 0; i < 4; i++ {
+					relaysChan <- peerstore.PeerInfo{ID: peer.ID(i)}
+				}
+
+				close(relaysChan)
+			}()
+
+			discover.EXPECT().FindPeers(gomock.Any(), echalotte.OnionRelay, gomock.Any()).Return(relaysChan, nil)
+
+			c, err := cb.Build(context.Background(), echalotte.CircuitTimeout(10*time.Millisecond))
+			assert.Error(t, err)
+			assert.True(t, strings.HasPrefix(err.Error(), echalotte.ErrFindRelays))
+			assert.True(t, strings.Contains(err.Error(), "channel"))
+			assert.Nil(t, c)
+		})
+
+		t.Run("times out when finding relay peers", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			discover := mocks.NewMockDiscovery(ctrl)
+			cb := newTestCircuitBuilder(t, discover)
+
+			// Simulate slow rate of finding relay peers.
+			// Timeout should kick in.
+			relaysChan := make(chan peerstore.PeerInfo)
+			go func() {
+				for i := 0; i < 100; i++ {
+					relaysChan <- peerstore.PeerInfo{ID: peer.ID(i)}
+					<-time.After(3 * time.Millisecond)
+				}
+
+				close(relaysChan)
+			}()
+
+			discover.EXPECT().FindPeers(gomock.Any(), echalotte.OnionRelay, gomock.Any()).Return(relaysChan, nil)
+
+			c, err := cb.Build(context.Background(), echalotte.CircuitTimeout(10*time.Millisecond))
+			assert.Error(t, err)
+			assert.True(t, strings.HasPrefix(err.Error(), echalotte.ErrFindRelays))
+			assert.True(t, strings.Contains(err.Error(), "timed out"))
+			assert.Nil(t, c)
+		})
+
+		t.Run("builds random circuit", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			discover := mocks.NewMockDiscovery(ctrl)
+			cb := newTestCircuitBuilder(t, discover)
+
+			relaysChan := make(chan peerstore.PeerInfo)
+			go func() {
+				for i := 0; i < 100; i++ {
+					relaysChan <- peerstore.PeerInfo{ID: peer.ID(i)}
+				}
+
+				close(relaysChan)
+			}()
+
+			discover.EXPECT().FindPeers(gomock.Any(), echalotte.OnionRelay, gomock.Any()).Return(relaysChan, nil)
+
+			c, err := cb.Build(context.Background(),
+				echalotte.CircuitSize(3),
+				echalotte.CircuitTimeout(10*time.Millisecond),
+			)
+			require.NoError(t, err)
+			require.Len(t, c, 3)
+			assert.NotSubset(t, c, []peer.ID{peer.ID(0), peer.ID(1), peer.ID(2)})
 		})
 	})
 }
